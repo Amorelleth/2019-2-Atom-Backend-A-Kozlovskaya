93bcbb2b5342d6082319fa13edd214a0010d60e6
Anna Kozlovskaya <a.kozlovskaya@corp.mail.ru>
2019-09-25 19:03:24 +0300

    init

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/a.kozlovskaya/atom-ff/2019-2-Atom-Backend-A-Kozlovskaya/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
